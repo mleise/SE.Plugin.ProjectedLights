be4d2527578dd593f515cdbcc197d053d8c3ce10
Marco Leise <Marco.Leise@gmx.de>
2024-03-07 21:15:51 +0100

    Added code to prevent double loading of the plugin (e.g. once from Plugin Hub and once via IDE).

--- Source/Plugin.cs
+++ Source/Plugin.cs
@@ -1,24 +1,66 @@
-﻿using HarmonyLib;
+﻿using avaness.PluginLoader;
+using HarmonyLib;
 using Sandbox.ModAPI;
+using System;
+using System.Collections.Generic;
 using System.Reflection;
 using VRage.Game.Components;
 using VRage.Plugins;
+using VRage.Utils;
 
 namespace mleise.ProjectedLightsPlugin
 {
 	sealed class Main : IPlugin
 	{
-		public void Startup() { }
-
-		public void Dispose() { }
-
+		/// <summary>Called when the plugins are instantiated.</summary>
 		public void Init(object gameInstance)
 		{
-			Harmony harmony = new Harmony("ProjectedLights");
+			// Ensure that we aren't loading twice.
+			var allPlugins = new List<KeyValuePair<IPlugin, bool>>();
+
+			foreach (var plugin in MyPlugins.Plugins)
+			{
+				if (plugin is avaness.PluginLoader.Main)
+				{
+					var pluginField = AccessTools.Field(typeof(PluginInstance), "plugin");
+					foreach (var pluginInstance in (List<PluginInstance>)AccessTools.Field(typeof(avaness.PluginLoader.Main), "plugins").GetValue(plugin))
+					{
+						allPlugins.Add(new KeyValuePair<IPlugin, bool>((IPlugin)pluginField.GetValue(pluginInstance), false));
+					}
+				}
+				else
+				{
+					allPlugins.Add(new KeyValuePair<IPlugin, bool>(plugin, true));
+				}
+			}
+
+			foreach (var plugin in allPlugins)
+			{
+				if (ReferenceEquals(plugin.Key, this))
+				{
+					break;
+				}
+				else if (plugin.Key.GetType().Namespace == typeof(Main).Namespace)
+		{
+					var assembly = plugin.Key.GetType().Assembly;
+					var location = assembly.Location;
+					var msg = "A version of \"" + assembly.GetCustomAttribute<AssemblyTitleAttribute>().Title + "\" has already been instantiated via the ";
+					msg += (plugin.Value ? "command-line" : "Plugin Loader") + ":" + (location == "" ? " Compiled from development folder." : "\n" + location);
+					MyMessageBox.Show("Plugin patching aborted", msg);
+					throw new Exception(msg);
+				}
+			}
+
+			// Patch Space Engineers.
+			Harmony harmony = new Harmony(typeof(Main).Namespace);
 			harmony.PatchAll(Assembly.GetExecutingAssembly());
 		}
 
-		public void Update() { }
+		/// <summary>Called when the game is closed.</summary>
+		public void Dispose() {}
+
+		/// <summary>Called at the end of each game update at 60 Hz, with only audio trailing it.</summary>
+		public void Update() {}
 	}
 
 	[MySessionComponentDescriptor(MyUpdateOrder.AfterSimulation)]
