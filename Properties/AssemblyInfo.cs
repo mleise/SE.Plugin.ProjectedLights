91ec2814922ef07fc781d70c3fd6014f6fd2d070
Marco Leise <Marco.Leise@gmx.de>
2024-03-11 03:26:09 +0100

    Prepare for v1.0.2.0

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // KEEP THESE UP-TO-DATE
 
 // Version that shows in the plugin list in Space Engineers as well as Windows Explorer unless overridden by an AssemblyFileVersion.
-[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyVersion("1.0.2.0")]
 // Version of Space Engineers this plugin was written for. Shows as "Product" related info in Windows Explorer.
 [assembly: AssemblyInformationalVersion("1.203.630 b0 default")]
 // Yearly updates to the copyright notice.
-[assembly: AssemblyCopyright("Copyright © Marco Leise 2024")]
+[assembly: AssemblyCopyright("Copyright © 2024 Marco Leise")]
 
 // INFORMATION SPECIFIC TO THIS PLUGIN (KEEP IN SYNC WITH PLUGIN HUB XML)
 
